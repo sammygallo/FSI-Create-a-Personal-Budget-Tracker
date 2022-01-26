053d38166af6a77e4f9b66cb819bd646dcc1dbb1
Sammy Gallo <sammy.gallo@PH-0060.lan>
2022-01-26 17:12:56 -0500

    First commit after moving local project folder

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+/*for(let i = weeklyExpenseQuestions.length-1; i >= 0; i--) {
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++) {
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}*/
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+    
+while(i < weeklyExpenseQuestions.length) {
+    i++;
+}
